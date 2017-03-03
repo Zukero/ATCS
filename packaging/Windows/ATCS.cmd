ae9a6695b0a4fe3ead8ab4a5d4774fd4d2e8238c
Zukero <kpochat@gmail.com>
2017-03-03 17:34:21 +0100

    v0.5.2 updated. Messed up something in scripts about paths containing spaces.

--- packaging/Windows/ATCS.cmd
+++ packaging/Windows/ATCS.cmd
@@ -8,13 +8,13 @@ set JAVA_OPTS=
 set ENV_FILE=%ATCS_DIR%ATCS.env.bat
 set MAIN_CLASS=com.gpl.rpg.atcontentstudio.ATContentStudio
 
-if exist %ENV_FILE% (
-  call %ENV_FILE%
+if exist "%ENV_FILE%" (
+  call "%ENV_FILE%"
 ) else (
-  echo REM set MAX_MEM=%MAX_MEM% > %ENV_FILE%
-  echo REM set JAVA=%JAVA% >> %ENV_FILE%
-  echo REM set JAVA_OPTS=%JAVA_OPTS% >> %ENV_FILE%
-  echo. >> %ENV_FILE%
+  echo REM set MAX_MEM=%MAX_MEM% > "%ENV_FILE%"
+  echo REM set JAVA=%JAVA% >> "%ENV_FILE%"
+  echo REM set JAVA_OPTS=%JAVA_OPTS% >> "%ENV_FILE%"
+  echo. >> "%ENV_FILE%"
 )
 
 start "" "%JAVA%" %JAVA_OPTS% -Xmx%MAX_MEM% -cp "%CP%" %MAIN_CLASS%
